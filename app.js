5f70963dfe087d07e6d78a838866dac2decf11b3
relf218 <winglong218@hotmail.com>
2018-01-27 15:12:31 -0500

    additional stuff/setup backend for sprint 2

--- app.js
+++ app.js
@@ -12,6 +12,11 @@ var logger = function(req, res, next){
 app.use(logger);
 */
 
+//view engine
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+
 //body parser middleware
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
@@ -19,8 +24,9 @@ app.use(bodyParser.urlencoded({extended: false}));
 
 //set static path
 app.use(express.static(path.join(__dirname, 'public')))
+
 app.get('/', function(req,res){
-    res.send('Hello World');
+    res.render('index');
 })
 
 app.listen(3000, function(){
