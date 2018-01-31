21a2b285d4f10c51b36981651f17336013623133
MatJackson <mjackson74u@gmail.com>
2018-01-30 22:49:10 -0500

    seperated database stuff into its own .js file, testing stuff

--- app.js
+++ app.js
@@ -1,16 +1,7 @@
 var express = require('express');
-var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
 var path = require('path');
 
-// database stuff
-mongoose.connect('mongodb://localhost/test');
-
-var db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function() {
-    // we're connected!
-});
 
 var app = express();
 
---
+++ database.js
@@ -0,0 +1,35 @@
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/test');
+
+// connection notifcations
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+    // we're connected!
+});
+
+// TEST STUFF
+var userSchema = mongoose.Schema({
+    name: String
+});
+
+// test method
+userSchema.methods.test = function() {
+    var testMessage = this.name
+        ? "User name is " + this.name
+        : "I don't have a name";
+    console.log(test);
+}
+
+// model tests
+var User = mongoose.model('User', userSchema);
+
+var testUser = new User({ name: 'Mathew' });
+console.log(testUser.name);
+
+testUser.save(function (err, testUser) {
+    if (err) return console.error(err);
+    testUser.test();
+});
+
+// END TEST STUFF
